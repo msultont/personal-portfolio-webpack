0b281a974c33c2fc152c89f70485692fc051d264
Sulton <msulton55@gmail.com>
2025-01-09 14:59:39 +0700

    create base template for react configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import './styles.css';
+
+
+export const App = () => {
+  return (
+    <div className="app">
+      Hello world! my name is sulton!
+    </div>
+  )
+}
