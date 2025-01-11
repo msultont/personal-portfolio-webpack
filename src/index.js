555731ce5aadc46191503581f0b08b474c073577
Sulton <msulton55@gmail.com>
2025-01-11 10:24:10 +0700

    create index.js template for react configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import App from './App';
 
 // Get the div with "root" as id added in index.html  
 const reactContainer = document.getElementById('root')
