06d53c27e515b62d9e6d0bf32767781f738dd1ea
Sulton <msulton55@gmail.com>
2025-03-06 13:31:32 +0700

    remove navbar

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -13,49 +13,6 @@ const HomePage = () => {
     <>
       <div className="fh5co-loader"></div>
 
-      {/* Navbar Start */}
-      <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <Link
-          to="/"
-          className="navbar-brand d-flex align-items-center px-4 px-lg-5"
-        >
-          <h2 className="m-0 text-primary">
-            <i className="fa fa-car me-3"></i>BPSA
-          </h2>
-        </Link>
-
-        <button
-          type="button"
-          className="navbar-toggler me-4"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="#fh5co-header" className="nav-item nav-link active">
-              Home
-            </a>
-            <a href="#fh5co-about" className="nav-item nav-link">
-              About
-            </a>
-            <a href="#fh5co-work" className="nav-item nav-link">
-              Works
-            </a>
-          </div>
-
-          <Link
-            to="/quote"
-            className="btn btn-primary py-4 px-lg-5 d-none d-lg-block"
-          >
-            Get A Quote<i className="fa fa-arrow-right ms-3"></i>
-          </Link>
-        </div>
-      </nav>
-      {/* Navbar End */}
-
       <div id="page">
         <section
           id="fh5co-header"
